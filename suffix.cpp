101cb08db191127c769e090a2c3a94f3e83da8cd
Vaibhav Agarwal <vaibhavagarwal550@gmail.com>
2020-06-30 21:54:58 +0530

    Update suffix.cpp
    Added comments

--- suffix.cpp
+++ suffix.cpp
@@ -3,10 +3,12 @@ using namespace std;
 #define sd(n) scanf("%lld",&n)
 #define ll long long
 #define pii pair<int,int>
+
+
 int main() {
 	string s;
 	cin>>s;
-	s+="$";
+	s+="$";   //added $ it will not affect suffix array as ascii value of $ is less.First value of suffix array is index of empty string n+1
 	
 	int n=s.size();
 	
@@ -19,27 +21,27 @@ int main() {
 	vector<int>p(n),c(n);
 	
 	for(int i=0;i<n;i++)
-	    p[i]=v[i].second;
+	    p[i]=v[i].second;   //stores index of starting character for unit length strings in sorted order (k=0)
 	  
-	c[p[0]]=0;
+	c[p[0]]=0;      //equivalence class for first is 0
 	for(int i=1;i<n;i++)
 	{
 	    if(v[i].first==v[i-1].first)
 	        c[p[i]]=c[p[i-1]];
 	    else
-	        c[p[i]]=c[p[i-1]]+1;
+	        c[p[i]]=c[p[i-1]]+1;  //equivalence class if two strings are same they correspond to same class else value will be greater
 	}
 	
 	int k=0;
-	while((1<<k)<n)
+	while((1<<k)<n)    //  for k=0 unit length then k=1  (length=2) then (length=4) and so on 
 	{
 	    vector<pair<pii,int>> v(n);
 	    for(int i=0;i<n;i++)
-	        v[i]={{c[i],c[(i+(1<<k))%n]},i};
+	        v[i]={{c[i],c[(i+(1<<k))%n]},i};   // for length 2^k+1 we split into two halves each of length 2^k
 	        
-	   sort(v.begin(),v.end());
-	   for(int i=0;i<n;i++)
-	    p[i]=v[i].second;
+	   sort(v.begin(),v.end());   // sorting on the basis of equivalence class 
+ 	   for(int i=0;i<n;i++)
+	    p[i]=v[i].second;   //generating suffix array p 
 	  
     	c[p[0]]=0;
     	for(int i=1;i<n;i++)
@@ -52,8 +54,9 @@ int main() {
     	k++;
 	}
 	
+	//after log2(n) iterations at most all equivalence classes will be distinct then we get suffix array
 	for(int i=0;i<n;i++)
-	    cout<<p[i]<<"\t"<<s.substr(p[i],n-p[i])<<"\n";
+	    cout<<p[i]<<"\t"<<s.substr(p[i],n-p[i])<<"\n";  
 	    
 	 return 0;
 	
